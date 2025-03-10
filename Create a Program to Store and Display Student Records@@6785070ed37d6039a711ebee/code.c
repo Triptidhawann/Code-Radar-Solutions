9ed9b956bbf2aac5eb1eef0d7df8bf96f6ba7b6f
Tripti dhawan <triptidhawan15@gmail.com>
2025-03-10 19:00:38 +0530

    Solution to Create a Program to Store and Display Student Records

--- Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
+++ Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
@@ -1,32 +1,24 @@
-#include <stdio.h>
-
-int main() {
-    struct student {
-        int rollno;
-        char name[50];
-        float attendence;
-    };
-
-    struct student stud[1000];
-    int n;
-    
-    scanf("%d", &n); 
-    
-    
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &stud[i].rollno);         
-        getchar();                             
-        scanf("%[^\n]", stud[i].name);          
-        scanf("%f", &stud[i].attendence);     
-    }
-
-   
-    for (int i = 0; i < n; i++) {
-        printf("Roll Number: %d\n", stud[i].rollno);  // Print roll number
-        printf("Name: %s\n", stud[i].name);           // Print name
-        printf("Marks: %.2f\n", stud[i].attendence);  // Print attendance marks
-    }
-
-    return 0;
+#include<stdio.h>
+int main()
+{
+    struct student{
+    int rollno;
+    char name[50];
+    float attendence;
+};
+struct student stud[1000];
+int n;
+scanf("%d",&n);
+for(int i=0;i<n;i++){
+    scanf("%d",&stud[i].rollno);
+    scanf("%s",&stud[i].name);
+    scanf("%f",&stud[i].attendence);
+}
+for(int i=0;i<n;i++){
+    printf("Roll Number: %d, ",stud[i].rollno);
+    printf("Name: %s, ",stud[i].name);
+    printf("Marks: %.2f\n",stud[i].attendence);
+}
+return 0;
 }
 
