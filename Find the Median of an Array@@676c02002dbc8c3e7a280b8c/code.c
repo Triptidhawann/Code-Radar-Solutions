398909b92d69fe2cdf8e41bc331ca163fb8938cf
Tripti dhawan <triptidhawan15@gmail.com>
2025-04-10 01:33:35 +0530

    Solution to Find the Median of an Array

--- Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
+++ Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
@@ -1,47 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Function to sort the array using Bubble Sort
-void sortArray(int arr[], int n) {
-    int temp;
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-1-i; j++) {
-            if (arr[j] > arr[j+1]) {
-                // Swap
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
+// Comparison function for qsort
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
 }
 
 int main() {
     int n;
-
-
+   
     scanf("%d", &n);
 
     int arr[n];
-
-
+   
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
     // Sort the array
-    sortArray(arr, n);
+    qsort(arr, n, sizeof(int), compare);
 
-    // Find and print median
-    float median;
+    // Calculate and print median
     if (n % 2 == 1) {
         // Odd number of elements
-        median = arr[n / 2];
+        printf("Median: %d\n", arr[n / 2]);
     } else {
         // Even number of elements
-        median = (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
+        float median = (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
+        printf("Median: %d\n", median);
     }
-
-    printf("%d\n", median);
 
     return 0;
 }
+
